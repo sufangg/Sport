1fbf7dc64433dc70a5df3ff0b110d9390808d73d
SCSM11 <225597@student.upm.edu.my>
2025-05-23 08:13:55 +0800

    done

---
+++ src/main/java/com/csc3402/project/sport/SportApplication.java
@@ -0,0 +1,13 @@
+package com.csc3402.project.sport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csc3402/project/sport/SportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc3402.project.sport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
