feb3e01166a7f0fcb17f3c89efe1a1b335b80d7d
Nor Irfan <sa00215@student.upm.edu.my>
2025-06-09 09:29:46 +0800

    disable security

--- src/main/java/com/csc3402/project/sport/SportApplication.java
+++ src/main/java/com/csc3402/project/sport/SportApplication.java
@@ -2,12 +2,11 @@ package com.csc3402.project.sport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SportApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SportApplication.class, args);
     }
-
 }
